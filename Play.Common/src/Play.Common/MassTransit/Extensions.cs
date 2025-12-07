5d82a925ce485560b5445fae59c2720bc1f747eb
KarimMoustamid <moustamid.teck@gmail.com>
2025-12-07 12:04:51 -0600

    Refactor to use shared `Play.Common.Settings` for `RabbitMQSettings`, add MassTransit extensions for RabbitMQ, and update package references for consistency

---
+++ Play.Common/src/Play.Common/MassTransit/Extensions.cs
@@ -0,0 +1,36 @@
+namespace Play.Common.Repositories.MassTransit
+{
+    using System.Reflection;
+    using global::MassTransit;
+    using global::MassTransit.Definition;
+    using global::MassTransit.MultiBus;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Settings;
+
+    public static class Extensions
+    {
+        public static IServiceCollection AddMassTransitWithRabbitMq(this IServiceCollection services)
+        {
+            services.AddMassTransit<IBus>(configure =>
+            {
+                configure.AddConsumers(Assembly.GetEntryAssembly());
+                configure.UsingRabbitMq((context, cfg) =>
+                {
+                    #region Setup
+                    var configuration = context.GetRequiredService<IConfiguration>();
+
+                    var serviceSettings = configuration
+                        .GetSection(nameof(ServiceSettings))
+                        .Get<ServiceSettings>() ?? new ServiceSettings();
+                    var rabbitMQSettings = configuration.GetSection(nameof(RabbitMQSettings)).Get<RabbitMQSettings>() ?? new RabbitMQSettings();
+                    #endregion
+
+                    cfg.Host(rabbitMQSettings.Host);
+                    cfg.ConfigureEndpoints(context, new KebabCaseEndpointNameFormatter(serviceSettings.ServiceName, false));
+                });
+            });
+            return services;
+        }
+    }
+}
