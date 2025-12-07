c7fa25717c111e7a540d4029885df47eae1c6486
KarimMoustamid <moustamid.teck@gmail.com>
2025-12-06 19:24:03 -0600

    Update assembly informational versions and enhance HTTP client retry policies

--- Play.Inventory/src/Play.Inventory/Program.cs
+++ Play.Inventory/src/Play.Inventory/Program.cs
@@ -3,9 +3,15 @@ using Play.Common.Repositories;
 using Play.Common.Settings;
 using Play.Inventory.Client;
 using Polly;
+using Polly.Timeout;
+using Polly.Extensions.Http;
+using Microsoft.Extensions.DependencyInjection;
 var builder = WebApplication.CreateBuilder(args);
 
-ServiceSettings serviceSettings = builder.Configuration.GetSection("ServiceSettings").Get<ServiceSettings>();
+ServiceSettings serviceSettings = builder.Configuration.GetSection("ServiceSettings").Get<ServiceSettings>()
+    ?? throw new InvalidOperationException("Missing required configuration: ServiceSettings section");
+
+Random jitterer = new Random();
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -18,7 +24,14 @@ builder.Services.AddMongo(builder.Configuration, serviceSettings.ServiceName );
 builder.Services.AddHttpClient<CatalogClient>(client =>
 {
     client.BaseAddress = new Uri("https://localhost:5001");
-}).AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(TimeSpan.FromSeconds(1)));
+}).AddTransientHttpErrorPolicy(
+        builder => builder.
+            Or<TimeoutRejectedException>()
+            .WaitAndRetryAsync(5, retryAttempt =>
+                TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)) + TimeSpan.FromMilliseconds(jitterer.Next(0, 1000)),
+                onRetry: (outcome, timespan, retryAttempt) =>
+                    Console.WriteLine($"Retry {retryAttempt} of {outcome.Result.StatusCode} after {timespan.TotalSeconds} seconds. Waiting...")))
+.AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(TimeSpan.FromSeconds(1)));
 
 builder.Services.AddSingleton<string>(serviceSettings.ServiceName);
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Play.Common.MongoDB.MongoRepository<>));
